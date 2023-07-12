3bea137c86a746085a2a9988d5aceb8f094f4e05
公彦赤屋先 <kimihikoakaya@gmail.com>
2023-07-12 20:53:16 +0900

    Independent dependency metadata

--- plugin_Kinect360/Kinect360.cs
+++ plugin_Kinect360/Kinect360.cs
@@ -21,6 +21,9 @@ namespace plugin_Kinect360;
 [ExportMetadata("Publisher", "K2VR Team")]
 [ExportMetadata("Version", "1.0.0.0")]
 [ExportMetadata("Website", "https://github.com/KinectToVR/plugin_Kinect360")]
+[ExportMetadata("DependencyLink", "https://docs.k2vr.tech/{0}/360/setup/")]
+[ExportMetadata("DependencySource", "https://download.microsoft.com/download/E/C/5/EC50686B-82F4-4DBF-A922-980183B214E6/KinectRuntime-v1.8-Setup.exe")]
+[ExportMetadata("DependencySourceSilent", null)]
 public class Kinect360 : KinectHandler.KinectHandler, ITrackingDevice
 {
     [Import(typeof(IAmethystHost))] private IAmethystHost Host { get; set; }
